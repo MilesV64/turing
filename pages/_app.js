58fa6fc0131ead23c654f09176543f8b618b5469
Miles Vinson <milesv302@gmail.com>
2020-12-02 17:16:00 -0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,94 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>tuRing</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+      
+      <header className="fixed top-0 blur border-b border-gray-100 w-full h-16 flex justify-center items-center">
+        <Link href="/">
+          <a className="flex items-center">
+            <Image src="/logo.png" width={26} height={26} />
+            <h3 className="font-extrabold text-xl ml-2 text-gray-800">tuRing</h3>
           </a>
+        </Link>
+      </header>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <div className="m-auto max-w-5xl px-10 mb-40 pt-16">
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <div className="flex justify-center items-center w-full mt-24">
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="flex-1 text-center max-w-lg">
+            <h2 className="text-5xl font-extrabold text-gray-800 mb-4">tuRing</h2>
+            <p className="text-lg font-normal text-gray-700"><b>Safety at your fingertips.</b> Explore our revolutionary line of smart rings designed to promote safety in a variety of situtions.</p>
+          </div>
+
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className="flex items-center w-full mt-32">
+          <div className="flex-1 bg-blue-100 h-96">
+            <p></p>
+          </div>
+
+          <div className="w-20"></div>
+
+          <div className="flex-1">
+            <h2 className="text-3xl font-extrabold text-gray-800 mb-4">Covid-19 Ring</h2>
+            <p className="mb-3 text-lg font-normal text-gray-700">Explore our revolutionary line of smart rings designed to promote safety in a variety of situtions.</p>
+            <Link href="/">
+              <a className="rounded-md float-left py-1 px-2 -mx-2 -my-1 hover:bg-blue-50 border-blue-600 text-lg font-normal text-blue-500 flex items-center">
+                <p className="mr-1">Learn more</p>
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+              </a>
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex items-center w-full mt-32">
+          <div className="flex-1">
+            <h2 className="text-3xl font-extrabold text-gray-800 mb-4">Self-Defense Ring</h2>
+            <p className="mb-3 text-lg font-normal text-gray-700">Explore our revolutionary line of smart rings designed to promote safety in a variety of situtions.</p>
+            <Link href="/">
+              <a className="rounded-md float-left py-1 px-2 -mx-2 -my-1 hover:bg-blue-50 border-blue-600 text-lg font-normal text-blue-500 flex items-center">
+                <p className="mr-1">Learn more</p>
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+              </a>
+            </Link>
+          </div>
+
+          <div className="w-20"></div>
+
+          <div className="flex-1 bg-pink-100 h-96">
+            <p></p>
+          </div>
+        </div>
+
+        <div className="flex items-center w-full mt-32">
+          <div className="flex-1 bg-yellow-100 h-96">
+            <p></p>
+          </div>
+
+          <div className="w-20"></div>
+
+          <div className="flex-1">
+            <h2 className="text-3xl font-extrabold text-gray-800 mb-4">Emergency Help Ring</h2>
+            <p className="mb-3 text-lg font-normal text-gray-700">Explore our revolutionary line of smart rings designed to promote safety in a variety of situtions.</p>
+            <Link href="/">
+              <a className="rounded-md float-left py-1 px-2 -mx-2 -my-1 hover:bg-blue-50 border-blue-600 text-lg font-normal text-blue-500 flex items-center">
+                <p className="mr-1">Learn more</p>
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+              </a>
+            </Link>
+          </div>
+        </div>
+        
+      </div>
+
     </div>
   )
 }
