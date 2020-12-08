53c47bacaa9e57795190a9d89590ef4437b9214d
Miles Vinson <milesv302@gmail.com>
2020-12-08 14:50:59 -0800

    img

--- pages/emergencyhelp.js
+++ pages/emergencyhelp.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Ring from '../components/ring'
+import CaptionImage from '../components/captionimage'
 
 export default function EmergencyHelp() {
   return (
     <Ring title="Senior Citizen Safety Ring" color="bg-yellow-100" img="/alertring.png">
         The Senior Citizen Safety Ring will have the ability to contact emergency contact/authorities when emergency protocols (optional). Examples of events that would trigger a contact include unhealthy vital readings, heart-rate spikes/drops, and physical injury detection. GPS tracking will be enabled when 911 is called.
+    
+        <CaptionImage img="/alertdrawing.jpeg">
+          The Senior Citizen Safety Ring in action
+        </CaptionImage>
+    
     </Ring>
   )
 }
