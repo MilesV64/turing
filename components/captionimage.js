cebcd8d84bc6496a6a626672163ca4289dfb60e4
Miles Vinson <milesv302@gmail.com>
2020-12-08 16:18:26 -0800

    add defense storyboard

--- components/captionimage.js
+++ components/captionimage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function CaptionImage(props) {
     return (
         <div className="mt-14 mb-20">
-          <img className="w-full border py-2 border-gray-200 rounded" src={props.img}></img>
+          <img className={props.className + " w-full border py-2 border-gray-200 rounded"} src={props.img}></img>
           <p className="text-center text-sm text-gray-500 font-light mt-4">{props.children}</p>
         </div>
     )
--- pages/selfdefense.js
+++ pages/selfdefense.js
@@ -7,6 +7,10 @@ export default function SelfDefense() {
     <Ring title="Self-Defense Ring" color="bg-pink-100" img="/defensering.png">
         The self defense ring is for people who would benefit from extra safety when walking alone at night for example. The ring will contain an in-house mechanic similar to the COVID-19 ring that shoots pepper spray when physical buttons have been interacted with. Pepper spray will be refilled in a home docking station/charger. The ring will also enable the option for GPS tracking which will be constantly shared with a selected emergency contact.
     
+        <CaptionImage img="/defensestoryboard.png" className="px-4 pb-0 pt-4">
+          A storyboard of the defense ring being used for protection
+        </CaptionImage>
+
         <CaptionImage img="/defensedrawing.jpeg">
           The self-defense ring in action
         </CaptionImage>
